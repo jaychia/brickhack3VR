446c7aa8fd1567a73856b77b7cf8aa032f9f7ffc
Joo Ho Yeo <jy396@Joos-MacBook-Pro.local>
2017-02-11 21:57:02 -0500

    view.py

--- clickr/clickr/views.py
+++ clickr/clickr/views.py
@@ -24,11 +24,25 @@ def receiveName (request):
 	 # return JsonResponse({"new_prof": new_prof, "prof_name": requestedProfName, "questions": requestedQuestion}
 	 # {"id": int, "text": String})
 
-	 return JsonResponse({'new_prof': new_prof, 'prof_name': requestedProfName, 'id':prof.id})
+	return JsonResponse({'new_prof': new_prof, 'prof_name': requestedProfName, 'id':prof.id})
 
 
 def receiveQuestion (request):
-	requestedClassName=request.Get.get('className')
 	requestedProfName=request.Get.get('profName')
-	requestedText = request.Get.get('text')
-	requestedOptions = request.Get.get('options')
+	prof = Professor.object.get(profName=requestedProfName)
+	requestedClassName=request.Post.get('className')
+	requestedText = request.Post.get('text')
+	requestedOptions = request.Post.get('options')
+	correctNumber = int(request.Post.get('correct'))
+
+	requestedQuestion = Question(className = requestedClassName, professor=prof, text=requestedText,active=true)
+	requestedQuestion.save()
+
+	for index in range(len(requestedOptions)):
+        requestedOption = Option(question= requestedQuestion,sequence=index, text=requestedOptions[index], correct =false)
+        if index==correctNumber:
+        	requestedOption.correct = true
+        requestedOption.save()
+
+    return JsonResponse({'prof_name': requestedProfName, 'prof':prof, 'question_name'=requestedQuestion.profName, 'question'=requestedQuestion})
+
