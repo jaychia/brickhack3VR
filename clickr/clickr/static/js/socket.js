981add001fc91638d4426b69c61989599782d922
jaychia <jaychia94@gmail.com>
2017-02-12 11:37:34 -0500

    aesthetics

--- clickr/clickr/static/js/socket.js
+++ clickr/clickr/static/js/socket.js
@@ -12,14 +12,17 @@
 		    console.log(data);
 		    console.log(data.activate);
 		    if (data.activate) {
-		    	var newqn = $("<div id=qn"+ data.question_id + "></div>");
+		    	var newqn = $("<div class='list-group' id=qn"+ data.question_id + "></div>");
 		    	$('#js-new-questions').append(newqn);
-		    	newqn.append($("<div >" + data.question_text + "</div>"));
+		    	newqn.append($("<div class='activeQn list-group-item active'>" + data.question_text + "</div>"));
 			    for (i in data.options) {
 			    	newqn.append(
-			    		$("<div class='optionSelect' text=" + data.options[i][0] + "qn=" + data.question_id + " >" + data.options[i][1] + "</div>")
+			    		$("<div class='optionSelect list-group-item' text=" + data.options[i][0] + "qn=" + data.question_id + " >" + data.options[i][1] + "</div>")
 			    	);
 			   	};
+			   	var pbar = $("<div class='progress progress-striped active'><div id='pbarinside' class='progress-bar' style='width: 0%'></div>");
+			   	newqn.append(pbar);
+			   	makeItMove($("#pbarinside"));
 		    } else {
 		    	$("#qn" + data.question_id).remove();
 		    }
@@ -35,3 +38,7 @@
 	    chat_socket.send(JSON.stringify(message));
 	    return false;
 	});
+
+	function makeItMove(pbar) {
+		pbar.animate({width: "100%"}, 10000);
+	}
